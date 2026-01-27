a0b211ff4af4f719f2784f18f2362d334de5b952
onctoberant <wavissara@gmail.com>
2026-01-27 16:33:12 +0700

    update acticity2

---
+++ Lab/app/src/main/java/com/example/a173_lableanandriod/MainActivity2.kt
@@ -0,0 +1,66 @@
+package com.example.a173_lableanandriod
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateSetOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a173_lableanandriod.ui.theme._173_LabLeanAndriodTheme
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+//            _173_LabLeanAndriodTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting2(
+                        name = "Android2",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+     //       }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+//    var inputText by remember { mutableStateOf("") }
+//
+ //   Column {
+ //       Text(
+ //           text = "Hello $name! say = ..." + inputText,
+ //           modifier = modifier
+//        )
+ //       TextField(
+ //           value = "",
+ //           onValueChange = {}
+ //       )
+  //  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    _173_LabLeanAndriodTheme {
+        Greeting2("Android")
+    }
+}
